29306164109c12dd09cb40a3cc7aa0e42e5aef3f
yino <m15829090357@163.com>
2022-04-25 11:02:42 +0800

    unit

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/yino/onceFlight
 
 go 1.17
-
-require github.com/zeromicro/go-zero v1.3.2 // indirect
--- once_flight_test.go
+++ once_flight_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/zeromicro/go-zero/core/syncx"
-
 	"github.com/yino/onceFlight"
 )
 
@@ -16,29 +14,6 @@ func TestNewOnceFlight(t *testing.T) {
 	round := 10
 	var wg sync.WaitGroup
 	barrier := onceFlight.NewOnceFlight()
-	wg.Add(round)
-	for i := 0; i < round; i++ {
-		go func() {
-			defer wg.Done()
-			// 启用10个协程模拟获取缓存操作
-			val, err := barrier.Do("get_rand_int", func() (interface{}, error) {
-				time.Sleep(time.Second)
-				return rand.Int(), nil
-			})
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Println(val)
-			}
-		}()
-	}
-	wg.Wait()
-}
-
-func TestZeroFlight(t *testing.T) {
-	round := 10
-	var wg sync.WaitGroup
-	barrier := syncx.NewSingleFlight()
 	wg.Add(round)
 	for i := 0; i < round; i++ {
 		go func() {
